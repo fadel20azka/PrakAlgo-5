71670075f4046c1cae49128a8fe4d394251c12e4
Fadel <fadel_azka@yahoo.com>
2020-11-29 08:05:45 +0700

    first commit

---
+++ tugas5_124200059.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+	int a,b,i, g,z ;
+	
+	cout<<"Gambar bangun datar"<<endl;
+	cout<<"1. Kotak bolong"<<endl ;
+	cout<<"2. Segitiga"<<endl ;
+	cout<<"Pilih : ";
+	cin>>a ;
+
+	
+	switch(a)
+	{
+		case 1:
+		cout<<"\nPanjang : ";
+		cin>>a ;
+		cout<<"Lebar : ";	
+		cin>>b ;
+		for (i=1 ; i<=a ; i++)
+		{
+		for (g=1 ; g<=b; g++)
+		{
+		if (i==1 || i==a || g==1 || g== b)
+			{
+		cout<<"*" <<" ";
+			}
+		else
+			{
+				cout<<"  ";
+			}
+		}
+		cout<<endl ;
+		}
+		break ;
+	
+		case 2 :
+		cout<<"Tinggi : ";
+		cin>>a ;
+		for(g= 1 ; g<=a ; g++)
+		{
+			z=1 ;
+		cout<<g ;
+		for(i=1 ; i< g ;i++)
+		{
+			cout<<" "<<g+(a*i)-z ;
+			z=z+i+1 ;
+		}
+			cout<<endl ;
+		}
+	}
+}
